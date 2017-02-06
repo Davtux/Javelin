d0b9147384d4c07cd433a0298332fd5d73a0d3b5
Marwen <sami.marwen.isetcom@gmail.com>
2017-02-06 20:52:32 +0100

    Integrating the conf class

--- src/fr/unilim/Config.java
+++ src/fr/unilim/Config.java
@@ -1,8 +1,13 @@
 package fr.unilim;
 
+import java.nio.file.Path;
+
 public class Config {
-	public static final String path_to_jpf = "";
-	public static final String path_to_properties = "";
+	public static final String PATH_TO_JPF = "...";
+	public static final String PATH_TO_PROPORTIES = "...";
+	public static final String LOG_DIR ="logs";
+	public static final String JPF_CONF_NAME ="config.jpf";
+	public static final String JPF_MAIN_CONF_NAME ="config/main_config.jpf";
 	
 
 }
