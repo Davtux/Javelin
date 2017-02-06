ecfc354e8007776ca2487a7d4e903456832eca6e
Marwen <sami.marwen.isetcom@gmail.com>
2017-02-06 17:39:15 +0100

    Creating test package

---
+++ src/fr/unilim/tests/MainTester.java
@@ -0,0 +1,13 @@
+package fr.unilim.tests;
+
+import fr.unilim.Logger;
+
+public class MainTester {
+
+	public static void main(String[] args) {
+		Logger l = new Logger("dfsfdsf");
+		l.w(Logger.INFO, "This is a test");
+
+	}
+
+}
