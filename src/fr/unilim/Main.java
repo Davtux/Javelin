6b9217069bc3d8664bcbb67f12698e84d3df6797
Romain Barrat <romain.barrat@etu.unilim.fr>
2017-02-07 16:20:13 +0100

    change parser : accept all characters

--- src/fr/unilim/Main.java
+++ src/fr/unilim/Main.java
@@ -12,6 +12,10 @@ import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.bind.DatatypeConverter;
 
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
@@ -21,7 +25,7 @@ import org.graphstream.stream.file.FileSinkGraphML;
 import org.graphstream.stream.file.FileSource;
 import org.graphstream.stream.file.FileSourceFactory;
 
-
+import scala.Char;
 import scala.inline;
 
 public class Main {
@@ -42,13 +46,24 @@ public class Main {
 		String charset = "UTF-8"; // or what corresponds
 		  BufferedReader in = new BufferedReader( 
 		      new InputStreamReader (new FileInputStream("test.json"), charset));
-		  String line;
+		  String line, group;
+		  Pattern pattern = Pattern.compile("\\\\u....");
+		  Pattern pattern2 = Pattern.compile("[0-9a-fA-F]{4}");
+		  Matcher matcher, matcher2;
+		  Integer i;
+		  int toto;
 		  while( (line = in.readLine()) != null) {
-			  line = line.replaceAll("\\\\u0026", "=");
-			  line = line.replaceAll("\\\\u0027", "'");
-			  line = line.replaceAll("\\\\u003c", "<");
-			  line = line.replaceAll("\\\\u003e", ">");
-			  line = line.replaceAll("\\\\u003d", ">");
+			  matcher = pattern.matcher(line);
+			  while(matcher.find()){  
+				  group = matcher.group(0);
+				  matcher2 = pattern2.matcher(group);
+				  while(matcher2.find()){
+					  line = line.replaceAll("\\\\u"+matcher2.group(0),String.valueOf((char)Integer.parseInt(matcher2.group(0),16)));
+				  }  
+			  }
+			  
+			 
+			  
 			  System.out.println(line);
 		  }
 	}
