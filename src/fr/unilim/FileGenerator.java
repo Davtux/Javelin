9eef7ce5a8a87922ae0958ae10694928a73895c3
Simon Garrelou <simsor@riseup.net>
2017-02-07 15:15:26 +0100

    Génération de fichiers à partir de templates

--- src/fr/unilim/FileGenerator.java
+++ src/fr/unilim/FileGenerator.java
@@ -1,8 +1,15 @@
 package fr.unilim;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 public class FileGenerator {
 	
@@ -12,12 +19,23 @@ public class FileGenerator {
 	
 	private Logger l;
 	
+	private Configuration conf;
+	
 	public FileGenerator(String classPath, String appletClassName, String packageName){
 		clsPath = classPath;
 		appletClsName = appletClassName;
 		this.packageName = packageName;
 		
 		l = new Logger(getClass().getName());
+		
+		conf = new Configuration(Configuration.VERSION_2_3_25);
+		conf.setDefaultEncoding("UTF-8");
+		try {
+			conf.setDirectoryForTemplateLoading(new File("templates/"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			l.w(Logger.ERR, "Impossible de spécifier le dossier de templates : " + e.getMessage());
+		}
 	}
 	
 	/**
@@ -28,11 +46,18 @@ public class FileGenerator {
 		try{
 		    PrintWriter writer = new PrintWriter(Config.JPF_CONF_NAME, "UTF-8");
 		    	
-		    writer.println("@include="+Config.JPF_MAIN_CONF_NAME);
-		    writer.println("target=fr.unilim.???.MainTester");
-		    writer.println("jdart.configs."+packageName+".symbolic.include="+packageName+".*;");
-		    writer.println("concolic.method.process="+packageName+"."+appletClsName+".process(apdu:fr.unilim.jc.APDU)");
-		    writer.println("concolic.method=process");
+		    Template tmpl = conf.getTemplate("jpf.properties");
+		    try {
+		    	Map<String, Object> root = new HashMap<String, Object>();
+		    	root.put("main_conf_name", Config.JPF_MAIN_CONF_NAME);
+		    	root.put("packageName", packageName);
+		    	root.put("appletClsName", appletClsName);
+		    	
+				tmpl.process(root, writer);
+			} catch (TemplateException e) {
+				e.printStackTrace();
+				l.w(Logger.ERR, "Une erreur s'est produite lors de l'expansion de la template : " + e.getMessage());
+			}
 		    
 		    writer.close();
 		    
@@ -48,51 +73,19 @@ public class FileGenerator {
 	public boolean generateMainFile(){
 		try{
 		    PrintWriter writer = new PrintWriter("Main.java", "UTF-8");
-		    /*
-		     * package fr.inria.lhs.tests;
-
-import fr.inria.lhs.tests.APDU;
-import javacard.framework.ISOException;
-import lhs.simu.jc.JCRE;
-
-public class MainTester {
-
-	public static void main(String[] args) {
-		System.out.println("+ In main! +");
-
-		
-		new JCRE();	//we init the run env for JavaCard
-		
-		APDU mAPDU = new APDU();
-		
-		//byte[] theBuffer = {MyFutureApplet.CLA_TESTAPPLET, MyFutureApplet.INS_INTERROGER_COMPTEUR, (byte) 0, (byte) 0, (byte) 5};
-		//run(new MyFutureApplet(), mAPDU);
-		
-		run(new fr.inria.lhs.emvas.EMVApplet(), mAPDU);
-
-
-	}
-	
-	public static void run(fr.inria.lhs.emvas.EMVApplet app, APDU apdu){
-		
-		System.out.println("+ Selecting ! +");
-		app.select();
-		try{
-			System.out.println("+ Processing ! +");
-			app.process(apdu);
-			apdu.JCRE_return();
-		}catch(ISOException e){
-			System.err.println(e.getReason());
-			apdu.JCRE_return_Exception(e.getReason());
-		}finally{
-			System.out.println("+ Deselecting ! +");
-			app.deselect();
-		}
-	}
-		
-
-}
-		     */
+		    
+		    Template tpl = conf.getTemplate("MainTester.java");
+		    
+		    try {
+		    	Map<String, Object> root = new HashMap<String, Object>();
+		    	root.put("packageName", packageName);
+		    	root.put("appletClsName", appletClsName);
+		    	
+		    	tpl.process(root, writer);
+		    } catch (TemplateException te) {
+		    	te.printStackTrace();
+		    	l.w(Logger.ERR, "Une erreur s'est produite lors de l'expansion de la template : " + te.getMessage());
+		    }
 		    
 		    writer.close();
 		    l.w(Logger.INFO, "Le fichier Main.java est bien créé !");
@@ -103,5 +96,31 @@ public class MainTester {
 		   return false;
 		}
 	}
+
+	public String getClsPath() {
+		return clsPath;
+	}
+
+	public void setClsPath(String clsPath) {
+		this.clsPath = clsPath;
+	}
+
+	public String getAppletClsName() {
+		return appletClsName;
+	}
+
+	public void setAppletClsName(String appletClsName) {
+		this.appletClsName = appletClsName;
+	}
+
+	public String getPackageName() {
+		return packageName;
+	}
+
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+	
+	
 
 }
--- src/fr/unilim/tests/MainTester.java
+++ src/fr/unilim/tests/MainTester.java
@@ -1,13 +1,17 @@
 package fr.unilim.tests;
 
+import java.nio.file.Paths;
+
 import fr.unilim.Logger;
+import fr.unilim.Master;
 
 public class MainTester {
 
 	public static void main(String[] args) {
-		Logger l = new Logger("dfsfdsf");
-		l.w(Logger.INFO, "This is a test");
-
+		Master master = new Master(Paths.get("/home/simon/monTest"), Paths.get("/home/simon/montestgen"), "APDU", "com.simsor.apdu");
+		master.generate();
+		
+		System.out.println("Generated successfully");
 	}
 
 }
