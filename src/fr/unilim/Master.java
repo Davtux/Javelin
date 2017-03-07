f98023b72629247e45cec5e2e0f4093ece5732d5
Marwen <sami.marwen.isetcom@gmail.com>
2017-03-07 17:01:09 +0100

    Adding 'execute' method to the master class in order to get a full abstraction layer, adding logger to the master class

--- src/fr/unilim/Master.java
+++ src/fr/unilim/Master.java
@@ -1,10 +1,16 @@
 package fr.unilim;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * Master class delegating file generation and running JPF to other classes
- *
+ * This class should be the only interface with the main class,
+ * This represents an abstraction layer
  */
 public class Master {
 	
@@ -26,21 +32,41 @@ public class Master {
 	 * The name of the package the APDU is in
 	 */
 	private String packageName;
+	
+	/**
+	 * The Logger
+	 */
+	private Logger l;
 
 	public Master(Path classPath, Path newProjectPath, String aPDUClassName, String packageName) {
 		this.classPath = classPath;
 		this.newProjectPath = newProjectPath;
 		this.aPDUClassName = aPDUClassName;
 		this.packageName = packageName;
+		l = new Logger(getClass().getName());
+		l.w(Logger.INFO, "Start: "+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
 	}
 	
 	/**
 	 * Generates the MainTest.java file as well as jpf.properties 
+	 * @return true Only when both config and the main files are generated successfully
 	 */
-	public void generate() {
+	private boolean generate() {
 		FileGenerator fg = new FileGenerator(classPath.toString(), aPDUClassName, packageName);
-		fg.generateConfigFile();
-		fg.generateMainFile();
+		return fg.generateConfigFile() && fg.generateMainFile();
+	}
+	
+	/**
+	 * Instantiate JPFRunner class in order to execute JPF
+	 * @param pathToJPF The path to the JPF executable on the host system
+	 */
+	public void execute(Path pathToJPF){
+		if(generate()){
+			JPFRunner jpfRunner = new JPFRunner(pathToJPF);
+			jpfRunner.runJPF(Paths.get(Config.JPF_CONF_NAME));
+		}else{
+			l.w(Logger.ERR, "Failed when generating files.");
+		}
 	}
 
 	public Path getClassPath() {
--- src/fr/unilim/tests/MainTester.java
+++ src/fr/unilim/tests/MainTester.java
@@ -8,8 +8,8 @@ import fr.unilim.Master;
 public class MainTester {
 
 	public static void main(String[] args) {
-		Master master = new Master(Paths.get("/home/simon/monTest"), Paths.get("/home/simon/montestgen"), "APDU", "com.simsor.apdu");
-		master.generate();
+		Master master = new Master(Paths.get("/home/marwen/monTest"), Paths.get("/home/marwen/montestgen"), "APDU", "com.zeruf.apdu");
+		master.execute(Paths.get("/home/marwen/inria/jDart-proj/jpf-core/build/Runjpf.jar"));
 		
 		System.out.println("Generated successfully");
 	}
