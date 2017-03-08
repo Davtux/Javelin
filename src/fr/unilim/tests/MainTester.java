eeed671e42f7def25867cc1eca741a7a1cf1d450
Marwen <sami.marwen.isetcom@gmail.com>
2017-03-08 16:53:17 +0100

    MainTester now should only call the master class

--- src/fr/unilim/tests/MainTester.java
+++ src/fr/unilim/tests/MainTester.java
@@ -9,7 +9,7 @@ public class MainTester {
 
 	public static void main(String[] args) {
 		Master master = new Master(Paths.get("/home/marwen/monTest"), Paths.get("/home/marwen/montestgen"), "APDU", "com.zeruf.apdu");
-		master.execute(Paths.get("/home/marwen/inria/jDart-proj/jpf-core/build/Runjpf.jar"));
+		master.execute(Paths.get("/home/marwen/inria/jDart-proj/jpf-core/bin/jpf"), Paths.get("/home/marwen/inria/jDart-proj/z3/build"));
 		
 		System.out.println("Generated successfully");
 	}
