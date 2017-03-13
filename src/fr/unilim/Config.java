150d4d53ae9f434d77a17ad0d818cf160d0577b7
Marwen <sami.marwen.isetcom@gmail.com>
2017-03-13 16:03:32 +0100

    Adding some properties (Paths)

--- src/fr/unilim/Config.java
+++ src/fr/unilim/Config.java
@@ -1,10 +1,11 @@
 package fr.unilim;
 
 public class Config {
-	public static final String PATH_TO_JPF = "...";
-	public static final String PATH_TO_PROPERTIES = "...";
+
+	public static final String SUT_SRC_FOLDER = "SUT/src/fr/unilim/talos/";
+	public static final String SUT_BIN_FOLDER = "SUT/build/";
 	public static final String LOG_DIR ="logs";
-	public static final String JPF_CONF_NAME ="config.jpf";
+	public static final String JPF_CONF_NAME ="SUT/config.jpf";
 	public static final String JPF_MAIN_CONF_NAME ="config/main_config.jpf";
 	
 	private Config() {}
