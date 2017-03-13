988ee496e801797bb08b68e4679332756ae2085e
Marwen <sami.marwen.isetcom@gmail.com>
2017-03-13 14:23:19 +0100

    update tests

--- src/fr/unilim/tests/MainTester.java
+++ src/fr/unilim/tests/MainTester.java
@@ -4,13 +4,17 @@ import java.nio.file.Paths;
 
 import fr.unilim.Logger;
 import fr.unilim.Master;
+import fr.unilim.SUTIntegrator;
 
 public class MainTester {
 
 	public static void main(String[] args) {
-		Master master = new Master(Paths.get("/home/marwen/monTest"), Paths.get("/home/marwen/montestgen"), "APDU", "com.zeruf.apdu");
+		/*Master master = new Master(Paths.get("/home/marwen/monTest"), Paths.get("/home/marwen/montestgen"), "APDU", "com.zeruf.apdu");
 		master.execute(Paths.get("/home/marwen/inria/jDart-proj/jpf-core/bin/jpf"), Paths.get("/home/marwen/inria/jDart-proj/z3/build"));
+		*/
 		
+		//SUTIntegrator suti = new SUTIntegrator(Paths.get("/home/marwen/test_dest"));
+		//suti.integrate(Paths.get("/home/marwen/test1"));
 		System.out.println("Generated successfully");
 	}
 
