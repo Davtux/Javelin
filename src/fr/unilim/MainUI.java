f0f6ed95f98004aacfd9fd29771973fd620a404a
Marwen <sami.marwen.isetcom@gmail.com>
2017-03-29 15:10:25 +0200

    Adding first UI, yohohoho !

--- src/fr/unilim/MainUI.java
+++ src/fr/unilim/MainUI.java
@@ -1,10 +1,25 @@
 package fr.unilim;
 
-public class MainUI {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class MainUI extends Application{
 
-	}
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("main_interface.fxml"));
+
+		Scene scene = new Scene(root, 650, 650);
+
+        primaryStage.setTitle("Javelin");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+ public static void main(String[] args) {
+        launch(args);
+    }
 
 }
