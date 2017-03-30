38c097168a3526ba572379347c691db3facd66c6
Jarrige Clément <clement.jarrige@etu.unilim.fr>
2017-03-30 18:15:59 +0200

    UI : main window with menu bar and Controller

---
+++ src/fr/unilim/application/gui/Controller.java
@@ -0,0 +1,11 @@
+package fr.unilim.application.gui;
+
+import javafx.application.Platform;
+
+public class Controller {
+	
+	
+	public void close(){
+		Platform.exit();
+	}
+}
--- src/fr/unilim/application/gui/MainUI.java
+++ src/fr/unilim/application/gui/MainUI.java
@@ -1,4 +1,8 @@
-package fr.unilim;
+package fr.unilim.application.gui;
+
+import java.io.IOException;
+
+import org.slf4j.LoggerFactory;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +11,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainUI extends Application{
+	
+	public final static org.slf4j.Logger log = LoggerFactory.getLogger(MainUI.class);
 
 	@Override
     public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("main_interface.fxml"));
+		Parent root = null;
+		try{
+			root = FXMLLoader.load(getClass().getResource("main_interface.fxml"));			
+		}catch(IOException ioe){
+			log.error("Can't load main_interface.fxml", ioe);
+			return;
+		}
 
 		Scene scene = new Scene(root, 650, 650);
 
@@ -18,7 +30,9 @@ public class MainUI extends Application{
         primaryStage.setScene(scene);
         primaryStage.show();
     }
- public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) {
         launch(args);
     }
 
