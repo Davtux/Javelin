53db6121abe0d56067291a414272ceeed4fa1a09
Jarrige Clément <clement.jarrige@etu.unilim.fr>
2017-04-14 15:06:39 +0200

    Fix DirectoryChooser NulllPointer

--- src/fr/unilim/application/gui/util/DirectoryChooserUtil.java
+++ src/fr/unilim/application/gui/util/DirectoryChooserUtil.java
@@ -20,14 +20,14 @@ public class DirectoryChooserUtil {
 	 * @param initial
 	 * 		if not null directory chooser is open in initial,
 	 * 		but if tf contains an exists path, this path is choose.
-	 * @return File selected 
+	 * @return File selected  
 	 */
 	public static File createDirectoryChooser(Window parent, String title, TextField tf, File initial){
 		DirectoryChooser chooser = new DirectoryChooser();
 		chooser.setTitle(title);
 		boolean initialSet = false;
 		
-		if(tf != null && !tf.getText().isEmpty()){
+		if(tf != null && tf.getText() != null && !tf.getText().isEmpty()){
 			File f = new File(tf.getText());
 			if(f.exists()){
 				chooser.setInitialDirectory(f);
