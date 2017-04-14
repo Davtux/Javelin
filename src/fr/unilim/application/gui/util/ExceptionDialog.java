62b4e0ee2b2173180f4be511d2919602e4602007
Jarrige Clément <clement.jarrige@etu.unilim.fr>
2017-04-14 15:46:13 +0200

    Show Stack trace in dialog

--- src/fr/unilim/application/gui/util/ExceptionDialog.java
+++ src/fr/unilim/application/gui/util/ExceptionDialog.java
@@ -1,6 +1,14 @@
 package fr.unilim.application.gui.util;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 
 public class ExceptionDialog {
 	
@@ -16,6 +24,32 @@ public class ExceptionDialog {
         alert.setTitle("Erreur");
         alert.setHeaderText(null);
         alert.setContentText(e.getMessage());
+        
+        // Create expandable Exception.
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String exceptionText = sw.toString();
+        
+        Label label = new Label("The exception stacktrace was:");
+		TextArea textArea = new TextArea(exceptionText);
+		textArea.setEditable(false);
+		textArea.setWrapText(true);
+		
+		textArea.setMaxWidth(Double.MAX_VALUE);
+		textArea.setMaxHeight(Double.MAX_VALUE);
+		GridPane.setVgrow(textArea, Priority.ALWAYS);
+		GridPane.setHgrow(textArea, Priority.ALWAYS);
+		
+		GridPane expContent = new GridPane();
+		expContent.setMaxWidth(Double.MAX_VALUE);
+		expContent.add(label, 0, 0);
+		expContent.add(textArea, 0, 1);
+		
+		// Set expandable Exception into the dialog pane.
+		alert.getDialogPane().setExpandableContent(expContent);
+		alert.setResizable(true);
+	        
         alert.showAndWait();
     }
 }
